a1f415c2d2611a8be09a01547355538409b9d33a
79tanka <72666478+79tanka@users.noreply.github.com>
2021-10-16 00:02:52 +0300

    homework-2

--- main.js
+++ main.js
@@ -1,23 +1,64 @@
 const arena=document.querySelector('.arenas');
+const randomButton=document.querySelector('.button');
 
 const player1={
+    number:'1',
     name:'Sonya',
-    hp:80,
+    hp:100,
     img:'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
     weapon: ['bazuka','gun','superGun'],
-    attack: function(){console.log(name+'fight..')},
+    attack: function(){console.log(this.name+' fight..')},
 }
 const player2={
+    number:'2',
     name:'Kitana',
-    hp:90,
+    hp:100,
     img:'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
     weapon: ['knife','gun'],
-    attack: function(){console.log(name+'fight..')},
+    attack: function(){console.log(this.name+' fight..')},
 }
 
-function createPlayer(player,{hp,name,img}){
+let turn=0;
+randomButton.textContent=player1.name+', lets fight..';
+randomButton.addEventListener('click',()=>{
+    turn++; 
+    if(turn%2===0){ 
+        playerFight(player2,player1,1);
+        randomButton.textContent=player1.name+', let\'s fight..';
+    }else{  
+        playerFight(player1,player2,2);
+        randomButton.textContent=player2.name+', let\'s fight..';
+    }
+})
+
+function  randomDamage(){
+    return Math.ceil(Math.random()*20);
+}
+
+function playerFight(fighter, player, num){
+    
+    player.hp-=randomDamage();
+    if (player.hp<=0){
+        player.hp=0;
+        life[`${num}`-1].textContent='';
+        life[`${num}`-1].style.width=player.hp+'%';
+        arena.appendChild(showWinner(fighter));
+        randomButton.disabled = true;
+        return;
+    }    
+    life[`${num}`-1].textContent=player.hp;
+    life[`${num}`-1].style.width=player.hp+'%';
+}
+function showWinner(winnerPlayer){
+    const winner=document.createElement('div');
+    winner.classList.add('winner');
+    winner.innerText=winnerPlayer.name+' wins';
+    return winner;
+}
+
+function createPlayer({number,hp,name,img}){
     const playerdiv=document.createElement('div');
-    playerdiv.classList.add(`${player}`);
+    playerdiv.classList.add(`player${number}`);
     playerdiv.innerHTML=`
     
         <div class="progressbar">
@@ -28,9 +69,9 @@ function createPlayer(player,{hp,name,img}){
             <img src=${img} />
         </div>
     `;
-    
-    arena.appendChild(playerdiv);
+    return playerdiv;
 }
- createPlayer('player1',player1);
- createPlayer('player2',player2);
 
+    arena.appendChild(createPlayer(player1));
+    arena.appendChild(createPlayer(player2));
+    const life=document.querySelectorAll('.life');
